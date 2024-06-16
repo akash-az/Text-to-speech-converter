398f8667fe1ff759759b020938e71115d9953909
akash-az <akashranjan.ar020@gmail.com>
2024-06-17 00:35:44 +0530

    Animations added

--- script.js
+++ script.js
@@ -21,3 +21,25 @@ document.querySelector("button").addEventListener('click',()=>{
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
 });
+
+gsap.from("h1",{
+    opacity:0,
+    x:100,
+    duration:2,
+    delay:1
+});
+
+gsap.from(".tarea,select",{
+    opacity:0,
+    x:-100,
+    duration:2,
+    delay:1.5
+});
+
+gsap.from("span,button",{
+    opacity:0,
+    duration:1,
+    delay:3.5
+});
+    
+
